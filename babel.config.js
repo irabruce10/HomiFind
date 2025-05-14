30a6c3ce6db2e6180ba32ae25980363b61f92c70
Bruce <irabruce10@gmail.com>
2025-05-14 13:52:04 +0200

    Deployment

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,35 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+//       'nativewind/babel',
+//     ],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
+    ],
+    plugins: [
+      [
+        'transform-inline-environment-variables',
+        {
+          include: [
+            'EXPO_PUBLIC_APPWRITE_PROJECT_ID',
+            'EXPO_PUBLIC_APPWRITE_ENDPOINT',
+            'EXPO_PUBLIC_APPWRITE_DATABASE_ID',
+            'EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID',
+            'EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID',
+            'EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID',
+            'EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID',
+          ],
+        },
+      ],
     ],
   };
 };
